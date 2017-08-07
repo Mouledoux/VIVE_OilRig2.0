a5fec718dc61f31f298468e0a8e153b72a795503
Eric Mouledoux <EricMouledoux@gmail.com>
2017-08-07 14:14:59 -0500

    So many things

---
+++ Assets/Scripts/VRButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Valve.VR.InteractionSystem.Interactable))]
+public class VRButton : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent OnClick;
+
+    private void HandHoverUpdate(Valve.VR.InteractionSystem.Hand hand)
+    {
+        if (hand.GetStandardInteractionButtonDown())
+        {
+            OnClick.Invoke();
+        }
+    }
+}
